2fc0a070e261edb94e1a494d504639dab842d420
Brandon Ytuarte <brandonmichealytuarte@gmail.com>
2026-04-15 15:10:17 -0400

    fixed file path errors

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ pg = st.navigation([st.Page("0_Home_Page.py"),
                     st.Page("3_Project_3.py"),
                     st.Page("4_Project_4.py")])
 
-st.page_icon="images\BMY_Logo_cropped 2.jpg"
-st.logo("images\BMY_Logo_cropped 2.jpg")
+st.page_icon="images/BMY_Logo_cropped 2.jpg"
+st.logo("images/MY_Logo_cropped 2.jpg")
 
 pg.run()
